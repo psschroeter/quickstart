8e7e2f9cdbd4ffa36d8d4c7647bcb1b78428114e
Peter Schroeter <peter.schroeter@rightscale.com>
2011-11-03 14:16:03 -0700

    more stuff

--- apacheqs/recipes/default.rb
+++ apacheqs/recipes/default.rb
@@ -7,4 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package 'httpd'
+package "httpd"
+
+service "httpd" do
+  action :start
+end
+
+template "/var/www/html/index.html" do
+  source "index.html.erb"
+  user root
+  group root
+  mode 0755
+end
