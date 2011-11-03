3b9d97f0646b6823f34fefbf0a898ac4840a0ac7
Peter Schroeter <peter.schroeter@rightscale.com>
2011-11-03 15:01:00 -0700

    bleh

--- apacheqs/metadata.rb
+++ apacheqs/metadata.rb
@@ -6,3 +6,10 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
 recipe 'apacheqs::default', 'Install Apache'
+
+
+attribute "quickstart/name",
+  :display_name => "Your name",
+  :description => "Your name: Visible to the world!",
+  :required => true,
+  :recipes => [ "apacheqs::default" ]
