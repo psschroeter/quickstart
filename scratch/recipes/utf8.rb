fc825e288d0c2e2cabe748d2d47a868e6dc3f8bc
Peter Schroeter <peter.schroeter@rightscale.com>
2014-05-05 15:57:08 -0700

    Add in remote file test as well

--- scratch/recipes/utf8.rb
+++ scratch/recipes/utf8.rb
@@ -8,3 +8,22 @@ unless platform_family?("windows")
     code "echo Привет from bash"
   end
 end
+
+location = platform_family?("windows") ? "C:\\file.out" : "/tmp/file.out"
+md5sum = "17a46374aaef6af9c4f3c533ab42bccd"
+remote_file location do 
+  source "http://s3.amazonaws.com/rightscale_rightlink_dev/5.8.8/patch/2.tgz"
+  backup false
+  checksum md5sum
+end
+
+# errors out if Encoding.default_internal set only?
+ruby_block "test checksum" do 
+  block do 
+    require 'digest/md5'
+    f = ::File.open(location,"rb")
+    actual = Digest::MD5.hexdigest(f.read)
+    raise "checksum mismatch expected #{md5sum} vs actual #{actual}" unless md5sum == actual
+  end
+end
+
