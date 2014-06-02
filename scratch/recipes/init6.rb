9f5fb299aa96a1c608cfee52f97c9e0ad1378da4
Peter Schroeter <peter.schroeter@rightscale.com>
2014-06-02 15:10:48 -0700

    Fix up init6 to only boot once

--- scratch/recipes/init6.rb
+++ scratch/recipes/init6.rb
@@ -1,2 +1,7 @@
-log "Issueing an init6 to simulate an early reboot"
-execute "init 6"
+if ::File.exists?("/var/lib/booted")
+  log "Skipping reboot, /var/lib/booted file indicates we already booted"
+else
+  log "Issueing an init6 to simulate an early reboot"
+  ::File.open("/var/lib/booted","w") {|f| f.puts(Time.now)}
+  execute "init 6"
+end
