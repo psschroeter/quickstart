830ad7be2d3e93dc896ffd2ad3f526c59cf6474e
Peter Schroeter <peter.schroeter@rightscale.com>
2016-09-15 16:46:02 -0700

    Try and break attributes

--- scratch/recipes/credentials.rb
+++ scratch/recipes/credentials.rb
@@ -2,4 +2,8 @@ log "Secret credential test"
 
 log "#{node[:scratch][:secret]}"
 
-node.set[:scratch][:foo3] = "Привет"
+ruby_block "set attribute" do
+  block do
+    node.set[:scratch][:foo2] = File.read("/tmp/xxx")
+  end
+end
